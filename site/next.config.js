fd3222facd525a47199e32defd03966b99514616
Bob Ellison <bob@bobellison.net>
2023-07-07 22:22:16 -0400

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,11 +2,11 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'Bob's Kasm Registry',
+    description: 'A registry providing tweaked kasm images',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://bobe99.github.io/kasm-registry/',
+    contactUrl: 'https://github.com/bobe/kasm-registry/issues',
   },
   reactStrictMode: true,
   swcMinify: true,
